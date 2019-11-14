49008ebd1e32b8795b375d0fad287da4e1a6c2fc
coconnell1 <coconnell1@ithaca.edu>
2019-11-13 20:40:38 -0500

    Very basic producer structure and outline in comments for own sake

--- ExampleAIModule/Producer.h
+++ ExampleAIModule/Producer.h
@@ -1,1 +1,11 @@
 #pragma once
+#include <BWAPI.h>
+
+
+class Producer
+{
+public:
+	/* Going to need many more functions, this is just to start*/
+	virtual void produce();
+	virtual void research();
+};
