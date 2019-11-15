be91e4364b70a97eda532ab20065af1d596145ef
Eugene Kuznetsov <Azurekuz@gmail.com>
2019-11-15 13:45:51 -0500

    Added SparCraft, Began UnitManager

--- ExampleAIModule/UnitManager.h
+++ ExampleAIModule/UnitManager.h
@@ -1,1 +1,11 @@
 #pragma once
+#include <BWAPI.h>
+#include <forward_list>
+#include "../SparCraft/source/SparCraft.h"
+
+class UnitManager {
+	std::forward_list<BWAPI::Unit> armyUnits;
+
+public:
+	virtual void addToMilitary(BWAPI::Unit newUnit);
+};
